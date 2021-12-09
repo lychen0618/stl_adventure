abefc17f53254af617ae0a170aa6fcf7c6ccfc1c
lychen <412369718@qq.com>
2021-12-09 11:17:23 +0800

    finish pool_allocator

--- memory/tests/simple_test.cpp
+++ memory/tests/simple_test.cpp
@@ -8,15 +8,16 @@ void handler(){
 }
 
 typedef sad::malloc_allocator<int> allocator;
+typedef sad::pool_allocator<int> double_allocator;
 
 int main(){
     allocator::set_oom_handler(handler);
-    int* arr = static_cast<int*>(allocator::allocate(2));
+    int* arr = static_cast<int*>(double_allocator::allocate(2));
     sad::construct(arr, 4);
     sad::construct(arr + 1, 3);
     std::cout << arr[0] << " " << arr[1] << std::endl;
     sad::destroy(arr);
     sad::destroy(arr + 1);
-    allocator::deallocate(arr, 2);
+    double_allocator::deallocate(arr, 2);
     return 0;
 }
--- small_test.cpp
+++ small_test.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
-#include <string>
-#include <list>
-using namespace std;
 
-int main(){
-    if(__has_trivial_destructor(int)) cout << "string type has trivial_destructor\n";
-    else cout << "string type does not have trivial_destructor\n";
+void get()
+{
+    int e;
+    int f = 4;
+    static int g;
+    static int h = 5;
+    std::cout << "in get(), the address a is " << &e << std::endl;
+    std::cout << "in get(), the address b is " << &f << std::endl;
+    std::cout << "in get(), the address c is " << &g << std::endl;
+    std::cout << "in get(), the address d is " << &h << std::endl;
+    while (1); // 让程序死在这用来查看内存映射表
+}
+
+int main()
+{
+
+    // int a = 1;
+    // int b;
+    // static int c = 2;
+    // static int d;
+    // int *p = new int(3);
+    // std::cout << "in main, the address a is " << &a << std::endl;
+    // std::cout << "in main, the address b is " << &b << std::endl;
+    // std::cout << "in main, the address c is " << &c << std::endl;
+    // std::cout << "in main, the address d is " << &d << std::endl;
+    // std::cout << "in main, the address p is " << p << std::endl;
+    // get(); // 程序在这进入死循环
+
+    // delete p;
+    // p = NULL;
+    int i = 0 + 10;
+    printf("i = %d\n", i);
+    while(true) ;
     return 0;
 }
